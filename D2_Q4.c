e9cfc6c78afd783700e1d39938e2aa07137bc9d7
Chaitanya <73275386+Chaitanya189@users.noreply.github.com>
2025-08-26 18:55:07 +0530

    Create D2_Q4.c

---
+++ D2_Q4.c
@@ -0,0 +1,19 @@
+// Write a program to calculate the area and circumference of a circle given its radius.
+
+#include<stdio.h>
+#include<conio.h>
+void main(){
+    float radii,area,circle;
+
+    printf("The radius of the circle is:");
+    scanf("%f",&radii);
+    
+    area=3.14*radii*radii;
+    printf("The area of the circle is %f sq units \n", area);
+    circle=2*3.14*r;
+    printf("The perimeter of the circle is %f sq units", circle);
+
+
+    getch();
+
+}
